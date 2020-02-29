307a7f632bb5af047fe060a787ad5bd612dc3af6
david <david.garcia.castao@gmail.com>
2020-02-29 14:41:36 +0100

    ejercicio

--- src/Git.java
+++ src/Git.java
@@ -3,7 +3,9 @@ public class Git {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola");
+		System.out.println("hola david");
+		System.out.println("hola david");
 	}
+	
 
 }
